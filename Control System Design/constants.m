ea457306e2129a7b2d4f274085d681e419b2640f
Onur Ulusoy <onurulusoys4@gmail.com>
2023-02-11 03:53:07 +0300

    pi-pd is designed for pure model

---
+++ Control System Design/constants.m
@@ -0,0 +1,8 @@
+clc;clear;
+%% Constants of the transfer function
+m = 0.11; % kg - mass of the ball
+R = 0.015; % m - radius of the ball
+d = 0.03; % m - distance from lever pin to the center of gear
+g = 9.81; % m/s^2 - gravitational acceleration
+L = 1; % m - length of the tube
+J = 9.9 * 10^-6; % kg*m^2 - mass moment of inertia of the ball
